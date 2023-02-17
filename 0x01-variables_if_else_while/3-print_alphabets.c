1a5f619848145b970f9325f730e565e3f262493c
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-17 16:41:41 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int chr;
+
+for (chr = 'a'; chr <= 'z'; chr++)
+	putchar(chr);
+for (chr = 'A'; chr <= 'Z'; chr++)
+	putchar(chr);
+putchar('\n');
+return (0);
+}
