e6ca30049cdc9a7ca42e9b012a0a648752873213
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-17 17:09:05 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+	putchar(digit%10 + '0');
+putchar("\n");
+return (0);
+}
