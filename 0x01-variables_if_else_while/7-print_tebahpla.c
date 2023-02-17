d1854e4abf0c0f1e1bd1abbae0b7bb050fd97a36
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-17 17:17:06 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int chr;
+
+for (chr = 'z'; chr >= 'a'; chr--)
+	putchar(chr);
+putchar('\n');
+return (0);
+}
