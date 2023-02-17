03a18ab923c6f020f5f058bef2f181a10f69d24f
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-17 17:48:40 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit <= 9; digit++)
+{
+putchar((digit % 10) + '0');
+if (digit == 9)
+	continue;
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
+
