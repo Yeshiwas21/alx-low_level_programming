b82cb94adfecbd6d0de2c9e5722d80ae6ce6984d
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-17 17:29:47 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+char alphabet;
+
+for (digit = 0; digit < 10; digit++)
+	putchar(digit % 10 + '0');
+for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	putchar(alphabet);
+putchar('\n');
+return 0;
+}
