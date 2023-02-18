8ea86663f2dfb7fd05c3fbf9bc7610fd1c622c6a
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-18 11:23:59 +0300

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+int num3;
+int num;
+
+for (digit = 0; digit < 10; digit++)
+{
+for (num = digit + 1; num < 10; num++)
+{
+for (num3 = num +1; num3 < 10; num3++)
+{
+if (digit != num && digit != num3 && num != num3 )
+{
+putchar((digit % 10) + '0');
+putchar((num % 10) + '0');
+putchar((num3 % 10) + '0');
+}
+if (digit != 7)
+{
+putchar(',');
+}
+putchar(' ');
+}
+}
+}
+return (0);
+}
