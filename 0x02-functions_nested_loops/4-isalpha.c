6ab713bbb51611808d1964ecb0813977c2097f5f
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-21 18:00:00 +0300

    a function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Yeshiwas Dagnaw Alemu
+ */
+
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+
+if (isalpha(c))
+        return (1);
+ielse
+	return (0);
+}
