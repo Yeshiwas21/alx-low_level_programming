d02b2dfddae610b879788c11f360eed4e2d7c0bd
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-22 11:21:41 +0300

    a function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,9 @@
 /*
- * File: 8-24_hours.c
+ * File: 7-print_last_digit.c
  * Auth: Yeshiwas Dagnaw Alemu
  */
 
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * print_last_digit - prints the last digit of a number.
@@ -13,13 +12,16 @@
  * Return: The value of the last digit.
  */
 
-void jack_bauer(void)
+int print_last_digit(int n)
 {
 int last_digit;
 last_digit = n % 10;
 
 if (last_digit < 0)
-	last_digit *= -1;
+		last_digit *= -1;
+
 _putchar(last_digit + '0');
+
 return (last_digit);
 }
+
