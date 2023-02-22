793bd2f19e271e382b8fc88ef7f21b063735adf9
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-22 17:11:49 +0300

    Fix the code in 2-largest_number.c so that it correctly prints out the largest of three numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,11 +13,11 @@ int largest_number(int a, int b, int c)
 int largest;
 
 if (a > b && a > c)
-	largest = a;
+	printf("%d is the largest number", a);
 else if (b > a && b > c)
-	largest = b;
+	printf("%d is the largest number", b);
 else
-	largest = c;
+	printf("%d is the largest number", c);
 return (largest);
 }
 
