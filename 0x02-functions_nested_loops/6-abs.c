3647602fccc7b923fa251a7dd5cbd27251f9cc70
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-22 10:40:25 +0300

    Administrator@DESKTOP-JMR591K MINGW64 /d/ALX Project/C programming/alx-low_level_programming/0x02-functions_nested_loops (main) $ git pull remote: Enumerating objects: 18, done. remote: Counting objects: 100% (18/18), done. remote: Compressing objects: 100% (10/10), done. remote: Total 16 (delta 11), reused 11 (delta 6), pack-reused 0 Unpacking objects: 100% (16/16), 1.48 KiB | 2.00 KiB/s, done. From https://github.com/Yeshiwas21/alx-low_level_programming    dde8968..871df06  main       -> origin/main Updating dde8968..871df06 Fast-forward  0x02-functions_nested_loops/5-sign.c | 32 ++++++++++++++++++++++++++++++++  1 file changed, 32 insertions(+)  create mode 100644 0x02-functions_nested_loops/5-sign.c
    Administrator@DESKTOP-JMR591K MINGW64 /d/ALX Project/C programming/alx-low_level_programming/0x02-functions_nested_loops (main)
    $ vim 6-abs.c
    
    Administrator@DESKTOP-JMR591K MINGW64 /d/ALX Project/C programming/alx-low_level_programming/0x02-functions_nested_loops (main)
    $ git add 6-abs.c
    warning: LF will be replaced by CRLF in 0x02-functions_nested_loops/6-abs.c.
    The file will have its original line endings in your working directory
    
    Administrator@DESKTOP-JMR591K MINGW64 /d/ALX Project/C programming/alx-low_level_programming/0x02-functions_nested_loops (main)
    $ git commit -m

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/*
+ * File: 6-abs.c
+ * Auth: Yeshiwas Dagnaw Alemu
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absulute value of an integer.
+ *
+ */
+
+int _abs(int n)
+{
+return abs(n);
+}
