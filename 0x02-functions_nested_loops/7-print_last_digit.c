06d5433c6192aedf6c6a3cf646eeff25e7057510
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-22 10:55:03 +0300

    a function that prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Yeshiwas Dagnaw Alemu
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @n: The integer to be computed.
+ *
+ * Return: The value of the last digit.
+ */
+
+int print_last_digit(int)
+{
+
+return (n % 10);
+}
