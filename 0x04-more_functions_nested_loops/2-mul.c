bb4dd86e2c681ef6fbdcc22aab127465ec2f72e5
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-23 13:55:52 +0300

    a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Auth: Yeshiwas Dagnaw Alemu
+ */
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first integer.
+ * @b: second integer
+ *
+ * Return: the product of two integers.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
