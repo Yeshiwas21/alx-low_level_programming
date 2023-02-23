2e20b3cf585b6d91c0f66b373e2966d58a3b3d38
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-23 14:06:35 +0300

    a function that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,13 @@
 
 /**
  * print_numbers - rints the numbers, from 0 to 9, followed by a new line.
- * @n: The character to be checked.
  *
  */
 
-int print_numbers(int n)
+void print_numbers(void)
 {
+int n;
+
 for (n = 0; n <= 9; n++)
 	_putchar((n % 10) + '0');
 _putchar('\n');
