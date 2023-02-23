4d1884d15d431ef95619e404777818d3a9eedc8b
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-23 14:26:07 +0300

    function that prints the numbers, from 0 to 9, followed by a new line except 2 and 4

---
+++ 0x03-debugging/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: Yeshiwas Dagnaw Alemu
+ */
+
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ *
+ */
+
+void print_most_numbers(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+if (n == 2 || n == 4)
+	continue;
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
+
