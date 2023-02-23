bfa52c4fe6ca0a15e796b6584dafd881aefed2e8
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-23 16:07:36 +0300

    a function that a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+/*
+ * File: 7-print_diagonal.c
+ * Auth: Yeshiwas Dagnaw Alemu
+ */
+
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line in the terminal.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+int line;
+int space;
+
+if (n > 0)
+{
+for (line = 1; line <= n; line++)
+{
+for (space = 1; space <= line; space++)
+    _putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
