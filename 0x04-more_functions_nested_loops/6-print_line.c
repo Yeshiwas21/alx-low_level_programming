9837369dbafb1d850eddea595c554a511c0a0a13
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-23 15:30:40 +0300

    a function that draws a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+/*
+ * File: 6-print_line.c
+ * Auth: Yeshiwas Dagnaw Alemu
+ */
+
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ *
+ */
+void print_line(int n)
+{
+int line;
+
+if (n > 0)
+{
+for (line = 0; line < n; line++)
+	_putchar('_');
+}
+_putchar('\n');
+}
