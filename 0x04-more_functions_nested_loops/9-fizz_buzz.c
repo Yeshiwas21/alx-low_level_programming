108a9dc0248949172919fa1e05019fd65d7f5fea
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-24 14:24:23 +0300

    Fiz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,12 +17,12 @@ int number;
 
 for (number = 1; number <= 100; number++)
 {
-if ((number % 3) == 0)
+if ((number % 3) == 0 && (number % 5) == 0)
+	printf("FizzBuzz");
+else if ((number % 3) == 0)
 	printf("Fizz");
 else if ((number % 5) == 0)
 	printf("Buzz");
-else if ((number % 3) == 0 && (number % 5) == 0)
-	printf("FizzBuzz");
 else
 	printf("%d", number);
 if (number == 100)
