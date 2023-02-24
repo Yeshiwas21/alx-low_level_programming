8afb5d5421640758f8b32dd52ad2608158bc888c
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-24 15:36:23 +0300

    a program that finds and prints the largest prime factor of the number 612852475143, followed by a new line

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>i
+
+/**
+  * main - Prints the largest prime factor of the number 612852475143
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+  unsigned long long n = 612852475143;
+  unsigned long long i;
+  for (i = 2ULL; i < n; i++) {
+  	//1ULL is 'unsigned long long
+    while (n % i == 0) {
+      n /= i;
+    }
+  }
+  printf("%llu\n", n);
+  return 0;
+}
