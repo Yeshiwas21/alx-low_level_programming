4149510d1e00957863e0309ba8c8f8f6d03cf56e
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-27 16:00:49 +0300

    a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+void _puts(char *str)
+{
+puts(str, stdout)
+_putchar('\n');
+}
