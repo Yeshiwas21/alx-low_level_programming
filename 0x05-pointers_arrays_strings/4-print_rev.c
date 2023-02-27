797c5cc93585a37fceb10003cf4ed56413ea04f4
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-27 16:32:03 +0300

    a function that prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,6 +9,13 @@
   */
 void print_rev(char *s)
 {
-return (strrev(s));
+int count = 0;
+
+while (s[count] != '\0')
+	count++;
+for (count = count - 1; count >= 0; count--)
+{
+_putchar(s[count]);
+}
 _putchar('\n');
 }
