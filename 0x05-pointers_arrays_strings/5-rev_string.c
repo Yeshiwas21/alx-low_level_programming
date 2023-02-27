9fe4971359dbe67d9797d0c0ebe1769ebbbb731a
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-27 17:00:32 +0300

    a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
+void rev_string(char *s)
+{
+char rev[1000];
+int i, j, count = 0;
+
+while (s[count] != '\0')
+	count++;
+j = count - 1;
+
+for (i = 0; i < count; i++)
+{
+rev[i] = s[j];
+j--;
+}
+}
