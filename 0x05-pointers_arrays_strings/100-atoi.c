ee216153df11b87319bb50821eed75b3cecc6f62
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-28 10:29:06 +0300

    a function that convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+/*
+ * File: 100-atoi.c
+ * Auth: Yeshiwas Dagnaw
+ */
+
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
+ */
+int _atoi(char *s)
+{
+int val;
+
+val = atoi(s);
+return (0);
+}
