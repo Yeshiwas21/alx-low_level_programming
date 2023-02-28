79852c284549dc7eaa2e6323ee322b07ae284b95
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-02-28 10:40:24 +0300

    a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,9 +15,18 @@
  */
 int _atoi(char *s)
 {
-int res = 0;
+int sign = 1;
+unsigned int n = 0;
 
-for (int i = 0; s[i] != '\0'; ++i)
-	res = res * 10 + s[i] - '0';
-return res;
+do
+{
+if (*s == '-')
+	sign *= -1;
+else if (*s >= '0' && *s <= '9')
+	n = (n * 10) + (*s - '0');
+else if (n > 0)
+	break;
+}
+while (*s++);
+return (n * sign);
 }
