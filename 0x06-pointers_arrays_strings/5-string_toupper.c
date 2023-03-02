b73f4dd4c743add52e5638bfd08ab08585add259
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-02 10:30:20 +0300

    a function that changes all lowercase letters of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * string_toupper - Changes all lowercase letters to uppercase
+  * @ch: the character to be converted to uppercase
+  *
+  * Return: the uppercase character corresponding to the ch
+  */
+char *string_toupper(char *)
+{
+char ch;
+return (touuper(ch));
+}
