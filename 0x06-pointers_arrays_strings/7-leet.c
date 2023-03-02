0b30e79620cbc4088d71922cc108503898f2ae96
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-02 11:31:28 +0300

    a function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+/**
+ * leet - encode string to 1337
+ * @s: string to encode
+ * Return: pointer to encoded string
+ */
+char *leet(char *s)
+{
+char *r = s;
+char chs[] = { 'a', 'e', 'o', 't', 'l' };
+char nums[] = { 4, 3, 0, 7, 1 };
+int i = 0;
+
+while (*s)
+{
+for (i = 0; i < 5; i++)
+{
+if (*s == chs[i] || *s == chs[i] - 32)
+	*s = nums[i] + '0';
+}
+s++;
+}
+return (r);
+}
+
