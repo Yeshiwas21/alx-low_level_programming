0e9213e1c6bcf2c55d60c63cc5e766ac32932478
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-02 11:13:14 +0300

    a function that capitilize a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *s)
 {
 int i;
 
-for(i=0; i < strlen(s); i++)
+for(i = 0; i < strlen(s); i++)
 {
 if (i == 0 || s[i-1] == ' ' && s[i] >= 'a' && s[i] <= 'z')
 	s[i] = toupper(s[i]);
