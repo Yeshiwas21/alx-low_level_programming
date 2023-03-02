c6d5e4cdb4c596e1c9f6cf690fb132f10f8d3324
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-02 11:10:03 +0300

    a function that capitilize a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
   * cap_string - capitalizes all words of a string.
-  * @str: the string to be capitilized
+  * @s: the string to be capitilized
   *
   * Return: the capitalized string
   */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 int i;
 
-for(i = 0; str[i] != '\0'; i++)
+for(i=0; i < strlen(s); i++)
 {
-if(i == 0)
-{
-if((str[i] >= 'a' && str[i] <= 'z'))
-	str[i] = str[i] - 32;
-continue;
+if (i == 0 || s[i-1] == ' ' && s[i] >= 'a' && s[i] <= 'z')
+	s[i] = toupper(s[i]);
 }
-if(str[i] == ' ')
-{
-++i;
-if(str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
-continue;
-}
-}
-else
-{
-if(str[i] >= 'A' && str[i] <= 'Z')
-	str[i] = str[i] + 32;
-}
-}
-return (str);
+return (s);
 }
