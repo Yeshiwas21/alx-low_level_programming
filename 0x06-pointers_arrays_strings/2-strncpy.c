3882ce6cff0230f351e5c074c4f97093b479397b
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-02 09:40:01 +0300

    a function that copies a string to a maximum of n characters

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,25 @@
+/*
+ * File: 2-strncpy.c
+ * Auth: Yeshiwas Dagnaw Alemu
+ */
 
+#include "main.h"
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
 #include "main.h"
 
 char *_strncpy(char *dest, char *src, int n)
 {
-
+strncpy(dest, src, n);
+return (dest);
 }
