9a28255900f1cd9147fb2742ee26d10a866c6a16
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-02 11:33:12 +0300

    a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,20 +5,19 @@
  */
 char *leet(char *s)
 {
-char *r = s;
 char chs[] = { 'a', 'e', 'o', 't', 'l' };
 char nums[] = { 4, 3, 0, 7, 1 };
 int i = 0;
 
-while (*s)
+while (s)
 {
 for (i = 0; i < 5; i++)
 {
-if (*s == chs[i] || *s == chs[i] - 32)
-	*s = nums[i] + '0';
+if (s == chs[i] || s == chs[i] - 32)
+	s = nums[i] + '0';
 }
 s++;
 }
-return (r);
+return (s);
 }
 
