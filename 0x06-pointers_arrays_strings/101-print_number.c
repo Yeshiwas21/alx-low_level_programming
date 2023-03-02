c6bfee41ef240e56a1d23e256b4042ab743e64c0
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-02 15:06:32 +0300

    a function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,13 +6,16 @@
   */
 void print_number(int n)
 {
+unsigned int num;
+
 if (n < 0)
-{    
+{
 _putchar('-');
-n = -n;
+n *= -1;
 }
-if (n / 10)
-	print_number(n / -10);
-_putchar(n % 10 + '0');
-}
+num = n;
+
+if (num / 10)
+	print_number(num / 10);
+_putchar(num % 10 + '0');
 }
