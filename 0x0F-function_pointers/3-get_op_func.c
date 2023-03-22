3109578ece0d316b036283d913d02d0856e284a4
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-22 17:04:40 +0300

    Getting operation functions

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - Find the proper operator to call the proper function
+ * @s: Operator to determine proper function to call
+ * Return: Pointer to function matching operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+int i;
+
+for (i = 0; i < 5; i++)
+{
+if (*ops[i].op == *s)
+	return (ops[i].f);
+}
+return (NULL);
+}
+Footer
+© 2023 GitHub, Inc.
