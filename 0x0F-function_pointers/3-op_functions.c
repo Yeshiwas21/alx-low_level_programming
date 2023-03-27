39aa02c95990834d8dadd94fca8d77ea1a28fc25
Yeshiwas21 <yeshiwasdagnaw23@gmail.com>
2023-03-27 01:34:56 -0700

    Modified files

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "3-calc.h"
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
  * op_add - Return sum of a & b
  * @a: First int to add
  * @b: Second int to add
  *
- * Return: Sum of a & b
+ * Return: (a + b)
  */
 int op_add(int a, int b)
 {
@@ -18,7 +22,7 @@ return (a + b);
  * @a: First int to sub
  * @b: Second itn to sub
  *
- * Return: Difference of a & b
+ * Return: (a - b)
  */
 int op_sub(int a, int b)
 {
@@ -30,7 +34,7 @@ return (a - b);
  * @a: First int to multiply
  * @b: Second int to multiply
  *
- * Return: Product of a & b
+ * Return: (a * b)
  */
 int op_mul(int a, int b)
 {
@@ -42,15 +46,10 @@ return (a * b);
  * @a: Int to divide
  * @b: Int to divide by
  *
- * Return: Quotient of a & b
+ * Return: (a / b)
  */
 int op_div(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
 return (a / b);
 }
 
@@ -59,14 +58,9 @@ return (a / b);
  * @a: Int to mod
  * @b: Int to mod by
  *
- * Return: Remainder of a & b
+ * Return: (a % b)
  */
 int op_mod(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
 return (a % b);
 }
