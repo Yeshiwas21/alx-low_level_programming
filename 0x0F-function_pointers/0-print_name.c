686eb6bc29e809571bf535acabec24f1e9c83664
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-03-28 17:14:13 +0300

    a function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * print_name - Prints a name.
  * @name: The name to be printed.
  * @f: A pointer ito a function that prints a name.
- * Return: Nothing:
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
