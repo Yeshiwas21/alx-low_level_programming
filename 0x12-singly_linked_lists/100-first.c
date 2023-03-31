40793c6be7b76c574a630c592cb8f81fbba98a8b
Yeshiwas21 <yeshiwasdagnaw23@gmail.com>
2023-03-31 00:52:27 -0700

    Display firt before main function gets executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
-#include "lists.h"
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
 
 /**
-* first - Print before main prints
-*
-* Return: void
-*/
-
+ * first - prints a sentence before the main
+ * function is executed
+ */
 void first(void)
 {
-printf("You're beat! and yet, you must allow,");
-printf("\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
+
