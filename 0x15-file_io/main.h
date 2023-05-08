fc224b8c266e48c10e8a48ede58d4cb5be004123
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-05-08 15:28:38 +0300

    Updated main file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,4 +10,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+/*3-cp.c*/
+void close_file_descriptor(int file_desc);
+char *allocate_buffer(char *filename);
+
+
 #endif
