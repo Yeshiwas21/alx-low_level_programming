a02a7b7c49354efea57c9f75c4b43f0c443074c7
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2023-06-15 16:29:20 +0300

    a function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,7 @@
 
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position
+ *
  * @h: Pointer to the doubly linked list
  * @idx: Index of the list where the new node should be added
  * @n: Value to be inserted
@@ -10,26 +11,16 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *new_node = NULL;
+	dlistint_t *current, *new_node;
 	unsigned int i;
 
-	if (h == NULL)
-		return (NULL);
-
-	current = *h;
-	for (i = 0; current && i < idx; i++)
-		current = current->next;
-
-	if (i != idx)
-		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || h == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = NULL;
 	new_node->next = NULL;
+	new_node->prev = NULL;
 
 	if (idx == 0)
 	{
@@ -37,15 +28,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (*h != NULL)
 			(*h)->prev = new_node;
 		*h = new_node;
+		return (new_node);
 	}
-	else
+
+	current = *h;
+	for (i = 0; current != NULL && i < idx - 1; i++)
+		current = current->next;
+
+	if (current == NULL)
 	{
-		new_node->next = current;
-		new_node->prev = current->prev;
-		current->prev->next = new_node;
-		current->prev = new_node;
+		free(new_node);
+		return (NULL);
 	}
+
+	new_node->next = current->next;
+	new_node->prev = current;
+
+	if (current->next != NULL)
+		current->next->prev = new_node;
+
+	current->next = new_node;
 
 	return (new_node);
 }
-
